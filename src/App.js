586f3dbd1876e112607a77e52a2209dda51a0977
Rebecca Mollica <rebecca.mollica@live.it>
2021-12-25 13:18:15 +0100

    feat: tasks input now working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 
+import { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
 import TodoList from './components/TodoList';
 
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
       <header>
-        <h1>ToDo list</h1>
+        <h1>ToDo List</h1>
       </header>
-      <Form />
-      <TodoList />
+      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
+      <TodoList setTodos={setTodos} todos={todos}/>
     </div>
   );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,19 @@
-const TodoList = () => {
+import Todo from './Todo';
+
+const TodoList = ({ todos, setTodos }) => {
     return (
         <div className="todo-container">
-            <ul className="todo-list"></ul>
+            <ul className="todo-list">
+                {todos.map(todo => (
+                    <Todo
+                        setTodos={setTodos}
+                        todos={todos}
+                        key={todo.id}
+                        todo={todo}
+                        text={todo.text}
+                    />
+                ))}
+            </ul>
         </div>
     );
 };
